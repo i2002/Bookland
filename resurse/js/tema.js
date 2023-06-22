2ad99c8a14fe1cc33af9349cca03bc65c28f1654
Tudor Butufei <tudor.butufei@gmail.com>
2023-06-22 14:40:47 +0300

    change theme with icon

--- resurse/js/tema.js
+++ resurse/js/tema.js
@@ -5,17 +5,34 @@ if (tema) {
     document.body.dataset.bsTheme = "dark";
 }
 
-window.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("switchTema").checked = tema == "dark";
-    document.getElementById("switchTema").onchange = function() {
-        if (document.body.classList.contains("dark")) {
+function setTheme(theme) {
+    document.getElementById("switchTema").checked = theme == "dark";
+    switch(theme) {
+        case "light":
             document.body.classList.remove("dark");
             document.body.dataset.bsTheme = "light";
+            document.querySelector("#theme-switch i").classList.add("fa-moon");
+            document.querySelector("#theme-switch i").classList.remove("fa-sun");
             localStorage.removeItem("tema");
-        } else {
+            break;
+
+        case "dark":
             document.body.classList.add("dark")
             document.body.dataset.bsTheme = "dark";
+            document.querySelector("#theme-switch i").classList.remove("fa-moon");
+            document.querySelector("#theme-switch i").classList.add("fa-sun");
             localStorage.setItem("tema","dark");
-        }
+            break;
     }
+}
+
+toggleTheme = function() {
+    let newTheme = document.body.classList.contains("dark") ? "light" : "dark";
+    setTheme(newTheme);
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    setTheme(tema == "dark" ? "dark" : "light");
+    document.getElementById("switchTema").onchange = toggleTheme;
+    document.querySelector("#theme-switch i").onclick = toggleTheme;
 });
